1e1b7261c79815d052840ba9de7233e54b883501
qhwgp <qhwgp@163.com>
2019-01-04 12:27:01 +0800

    test for giPython

--- gitPython.py
+++ gitPython.py
@@ -7,14 +7,21 @@ Created on Fri Jan  4 09:02:36 2019
 
 import git
 
-gitPath='F:/草稿/gitCode/gitCode'
 
-repo = git.Repo(gitPath)
 
-if repo.is_dirty():
-    fileList=[]
-    listStatus=repo.git.status().split('\n')
-    for strInfo in listStatus:
-        if '\tmodified:' in strInfo:
-            fileList.append()
-            
+def pushPath(gitPath,strUpdateInfo):
+    repo = git.Repo(gitPath)
+    if repo.is_dirty():
+        fileList=[]
+        listStatus=repo.git.status().split('\n')
+        for strInfo in listStatus:
+            if '\t' in strInfo:
+                fileList.append(strInfo.replace('\t','').replace('modified:   ',''))
+    repo.index.add(fileList)
+    repo.index.commit(strUpdateInfo)
+    repo.remote().push()
+    return fileList
+    
+if __name__=='__main__':
+    gitPath='F:/草稿/gitCode/gitCode'
+    fileList=pushPath(gitPath,'test for giPython')
