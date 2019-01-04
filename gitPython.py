750137348c9cb32391b5d939a2f68ea03ac10d27
qhwgp <qhwgp@163.com>
2019-01-04 12:34:57 +0800

    test OK for use

--- gitPython.py
+++ gitPython.py
@@ -11,16 +11,16 @@ import git
 
 def pushPath(gitPath,strUpdateInfo):
     repo = git.Repo(gitPath)
+    fileList=[]
     if repo.is_dirty():
-        fileList=[]
         listStatus=repo.git.status().split('\n')
         for strInfo in listStatus:
             if '\t' in strInfo:
                 fileList.append(strInfo.replace('\t','').replace('modified:   ',''))
-    if fileList:
-        repo.index.add(fileList)
-        repo.index.commit(strUpdateInfo)
-        repo.remote().push()
+        if fileList:
+            repo.index.add(fileList)
+            repo.index.commit(strUpdateInfo)
+            repo.remote().push()
     return fileList
     
 if __name__=='__main__':
