531aa69f9d164cae41f541489f357bac9ca7f0f4
wap <qhwgp@163.com>
2019-01-24 23:08:22 +0800

    by home at 2019-01-24 23:08:21

---
+++ git_pull.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan  7 08:43:52 2019
+
+@author: WGP
+"""
+import git,os
+gitPath='F:/草稿/gitCode/gitCode'
+if not os.path.exists(gitPath):
+    gitPath='C:/Users/WAP/gitCode'
+repo = git.Repo(gitPath)
+try:
+    repo.remote().pull()
+    print('pull from gitHub OK.')
+except:
+    repo.git.stash()
+    repo.remote().pull()
+    print('save work and pull from gitHub OK.')
---
+++ git_push.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan  4 09:02:36 2019
+
+@author: WGP
+
+push to git
+"""
+
+import git,datetime,os
+
+strUpdateInfo='by office at '+datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+gitPath='F:/草稿/gitCode/gitCode'
+if not os.path.exists(gitPath):
+    gitPath='C:/Users/WAP/gitCode'
+    strUpdateInfo='by home at '+datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+#-------------------------------------------
+repo = git.Repo(gitPath)
+fileList=[]
+listStatus=repo.git.status().split('\n')
+for strInfo in listStatus:
+    if '\t' in strInfo:
+        fileList.append(strInfo.replace('\t','').replace('modified:   ',''))
+if fileList:
+    repo.index.add(fileList)
+    repo.index.commit(strUpdateInfo)
+    repo.remote().push()
+    print('push to gitHub OK.')
+else:
+    print('Nothing happened.')
